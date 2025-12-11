00d151f9afe42a3ceccbc860ce63d9e5dcc3040c
adi23kharat <adityakhkharat1234@gmail.com>
2025-12-12 00:38:33 +0530

    server updates

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,11 @@
-import app from "../src/app.js";
-import connectDB from "../src/db/db.js";
-import serverless from "serverless-http";
-import dotenv from "dotenv";
+// import app from "../src/app.js";
+// import connectDB from "../src/db/db.js";
+// import serverless from "serverless-http";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-// Connect database only once
-connectDB();
+// // Connect database only once
+// connectDB();
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
